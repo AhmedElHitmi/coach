3bdab386ad030e95da4a1b576ea2d2012d0853f2
Ahmed <hitmi6511@gmail.com>
2016-08-01 15:29:27 +0200

    test js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+#test(){
+
+var photo = $(document).ready(){
+  $(this).attr("src");
+  $("resa.html").append("src",photo);
+  $("resa.html").show();
+  $(document).write("resa.html");
+};
+};
--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+//1. dans formulaire conseils quand click sur bouton réservez votre séance je recupere la photo et le nom du coach
+$(document).ready(function(test){
 
+$(".btn").click(function(){
+//2. je met dans le formulaire resa la photo et le nom
+  var $photo = $(this).prevUntil("div").attr("src");
+  var $nom = $(this).prevUntil(".nomcoach").text();
+
+  console.log($photo);
+  console.log($nom);
+
+
+
+
+})
+// $("resa.html").val("#photo_coach.src",$photo);
+// $("resa.html").val("nom_coach.text",$nom);
+//
+
+//3. j affiche le formulaire resa.
+// $("resa.html").show;
+});
