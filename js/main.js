19c55e9c385158040c9e725ba7d0df50e15db654
Ahmed <hitmi6511@gmail.com>
2016-08-01 17:56:40 +0200

    create a style to a photo in resa.html

--- js/main.js
+++ js/main.js
@@ -1,27 +1,21 @@
 //1. dans formulaire conseils quand click sur bouton réservez votre séance je recupere la photo et le nom du coach
 $(document).ready(function(test){
-//je recupere la photo et le nom du coach
-$(".btn").click(function(){
 
+$(".btn").click(function(){
+//je recupere la photo et le nom du coach
   var $photo = $(this).prevAll(".imgcoach").attr("src");
   var $nom = $(this).prevAll(".nomcoach").text();
+
+//je stock la photo et le nom du coach dans photo et nom dans sessionStorage.setItem
   sessionStorage.setItem("photo",$photo);
   sessionStorage.setItem("nom",$nom);
 })
-//2. je met dans le formulaire resa la photo et le nom
+//je verifie si j ai charger la page resa
 
 if ($("body").attr("class") === "resa") {
+  //si ok je recupere la photo et le nom dans sessionStorage.getItem et je les met dans src et #nom_coach
   $("img").attr("src",sessionStorage.getItem("photo"));
   $("#nom_coach").text(sessionStorage.getItem("nom"));
 };
 
-
-// $("resa.html").val("#photo_coach.src",$photo);
-
-
-// $("resa.html").val("nom_coach.text",$nom);
-//
-
-//3. j affiche le formulaire resa.
-// $("resa.html").show;
 });
