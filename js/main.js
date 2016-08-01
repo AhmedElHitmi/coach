2303d3cdfeeb8814febeae6d540bc080ef9acb85
Ahmed <hitmi6511@gmail.com>
2016-08-01 16:53:31 +0200

    create a button in resa.html

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 //1. dans formulaire conseils quand click sur bouton réservez votre séance je recupere la photo et le nom du coach
 $(document).ready(function(test){
-
+//je recupere la photo et le nom du coach
 $(".btn").click(function(){
-//2. je met dans le formulaire resa la photo et le nom
-  var $photo = $(this).prevUntil("div").attr("src");
-  var $nom = $(this).prevUntil(".nomcoach").text();
 
-  console.log($photo);
-  console.log($nom);
-
-
-
-
+  var $photo = $(this).prevAll(".imgcoach").attr("src");
+  var $nom = $(this).prevAll(".nomcoach").text();
+  sessionStorage.setItem("photo",$photo);
+  sessionStorage.setItem("nom",$nom);
 })
+//2. je met dans le formulaire resa la photo et le nom
+
+if ($("body").attr("class") === "resa") {
+  $("img").attr("src",sessionStorage.getItem("photo"));
+  $("#nom_coach").text(sessionStorage.getItem("nom"));
+};
+
+
 // $("resa.html").val("#photo_coach.src",$photo);
+
+
 // $("resa.html").val("nom_coach.text",$nom);
 //
 
